87d0066c0b25e6cc5cb505c024acfde4f162696c
digitz20 <alaekekaebuka200@gmail.com>
2025-03-24 09:23:14 +1345

    new

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ const options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'My API: https://assignmentluxe.onrender.com ',
+      title: 'My API: https://assignmentluxe.onrender.com',
       version: '1.0.0',
     },
     components: {
